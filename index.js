fab8231ae2521161ced2f27bde91da0c9cda51b5
Nikhil p <nikhilppry2001@gmail.com>
2024-09-07 00:01:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const changelogo = () => {
+    const logo = document.getElementById('logo');
+    logo.src = 'nikhil.jpg'; 
+};
+
+
+const reset = () => {
+    const logo = document.getElementById('logo');
+    logo.src = 'nikhz.webp';  
+};
